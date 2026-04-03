b490bd59492db4af840b5f8032ae797caaf6ddb0
Vishanan Sajeepan <22501689@me.uvsq.fr>
2026-04-03 15:04:54 +0200

    Ajout du test des soustraction

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -14,7 +14,7 @@ class PyCalcUnitTests(unittest.TestCase):
         got = calculator.add(l, r)
         self.assertEqual(got, e)
 
-def test_multiply(self):
+  def test_multiply(self):
     left = [5, 2, -3, -7, 0, -5]
     right = [9, -4, 0, -8, 0, 15]
     expected = [45, -8, 0, 56, 0, -75]
@@ -24,6 +24,18 @@ def test_multiply(self):
       e = expected[i]
       with self.subTest(l=l, r=r, e=e):
         got = calculator.multiply(l, r)
+        self.assertEqual(got, e)
+      
+  def test_subtract(self):
+    left = [5, 2, -3, -7, 0, -5]
+    right = [9, -4, 0, -8, 0, 15]
+    expected = [-4, 6, -3, 1, 0, -20]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.subtract(l, r)
         self.assertEqual(got, e)
 
 if __name__ == '__main__':
