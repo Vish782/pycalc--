d32d5950b25c9130d47a129103d1a031ccbc1e13
Vishanan Sajeepan <22501689@me.uvsq.fr>
2026-04-03 15:39:26 +0200

    Modification de la fonction substract

--- pycalc/pycalc.py
+++ pycalc/pycalc.py
@@ -1,18 +1,29 @@
 def add(a, b):
-    res = a
-    if b > 0:
-        while (b - 1) >= 0:
-            b -= 1
-            res += 1
-    elif b < 0:
-        while (b + 1) <= 0:
-            b += 1
-            res -= 1
-    return res
+  res = a
+  if b > 0:
+    while (b - 1) >= 0:
+      b -= 1
+      res += 1
+  elif b < 0:
+    while (b + 1) <= 0:
+      b += 1
+      res -= 1
+  return res
 
 def subtract(a, b):
-  return a - b
+  res = a
+  if b > 0:
+    while (b - 1) >= 0:
+      b -= 1
+      res -= 1
+  elif b < 0:
+    while (b + 1) <= 0:
+      b += 1
+      res += 1
+  return res
+
 def multiply(a, b):
   return a * b
+  
 def divide(a, b):
   return a / b
